977d724f037b59b88354b5e00069fa059230e4e8
SigmaHyperon <nano_tech@web.de>
2019-06-13 10:48:38 +0200

    added video added multiuser support

--- app.js
+++ app.js
@@ -10,30 +10,62 @@ app.use('/', express.static('www'));
 const server = http.createServer(app).listen(3000);
 io.listen(server);
 
-const clients = [];
-const pool = [];
-const partners = [];
+const pool = {};
+const partners = {};
 
-const relay = fn(self => target => data => {
-	if(target != null){
-		target.socket.emit("data", data);
-		console.log(`relay from ${self} to ${target.id}`);
-	} else {
-		throw `no partner selected`;
+const relay = fn(self => data => {
+	if(typeof partners[self] != 'undefined'){
+		partners[self].socket.emit("data", data);
 	}
 });
 
-const init = () => {
+const init = fn(id => () => {
+	if(Object.keys(pool).length > 1){
+		const a = pool[id];
+		delete pool[id];
+		const b_ID = Object.keys(pool)[Math.floor(Math.random()*Object.keys(pool).length)];
+		const b = pool[b_ID];
+		delete pool[b_ID];
+		partners[a.id] = b;
+		partners[b.id] = a;
+		a.socket.emit("init");
+	}
+});
 
-};
+const destroy = fn(id => () => {
+	if(typeof pool[id] != 'undefined')
+		delete pool[id];
+	if(typeof partners[id] != 'undefined'){
+		const b_ID = partners[id].id
+		pool[b_ID] = partners[b_ID];
+		delete partners[b_ID];
+		delete partners[id];
+	}
+});
+
+const rtcDisconnect = fn(id => () => {
+	if(typeof partners[id] != 'undefined'){
+		const a_ID = id;
+		pool[a_ID] = partners[a_ID];
+		const b_ID = partners[id].id
+		pool[b_ID] = partners[b_ID];
+		delete partners[b_ID];
+		delete partners[a_ID];
+	}
+});
 
 io.on('connection', socket => {
 	const id = uuid();
 	pool[id] = {id, socket};
-	let partner = null;
-	const relay_ = relay(id, partner);
+
+	const relay_ = relay(id);
+	const init_ = init(id);
+	const destroy_ = destroy(id);
+	const rtcDisconnect_ = rtcDisconnect(id);
 
 	socket.on("data", relay_);
-	socket.on("connect", init);
+	socket.on("init", init_);
+	socket.on("disconnect", destroy_);
+	socket.on("rtcDisconnect", rtcDisconnect_);
 })
 
--- www/index.js
+++ www/index.js
@@ -1,49 +1,65 @@
 let soc = null;
 let peer = null;
 let initiator = false;
+let video = null;
+const emit = (key, data) => (soc != null) ? soc.emit(key, data) : null;
+const destroyRTC = () => {
+	peer.destroy();
+	peer = null;
+	emit("rtcDisconnect");
+	console.warn("rtc connection destroyed");
+}
 function postData(data){
 	console.log(data);
 	$("div#received").append(`<p>${data}</p>`);
+}
+function createPeer(init = false){
+	const tempPeer = new SimplePeer({ initiator: init });
+	tempPeer.on('signal', data => {
+		console.log("signal", data);
+		emit('data', data);
+	});
+	tempPeer.on('data', postData);
+	
+	tempPeer.on('close', destroyRTC);
+	tempPeer.on('error', destroyRTC);
+	tempPeer.on('stream', stream => {
+		video.srcObject = stream;
+		video.play();
+	})
+
+	navigator.getUserMedia({ video: true, audio: false }, (stream) => {tempPeer.addStream(stream)}, () => {})
+	return tempPeer;
 }
 function init(){
 	const socket = io();
 	socket.on('connect', ()=>{
 		soc = socket;
 	});
+	socket.on("init", () => {
+		peer = createPeer(true);
+	});
 	socket.on('data', data => {
-		console.log("received data");
-		if(initiator == false){
-			if(peer == null){
-				peer = new SimplePeer();
-				peer.on('signal', d => {
-					socket.emit('data', d);
-				});
-				peer.on('data', postData);
-			}
-			peer.signal(data);
-		} else if( initiator == true) {
-			peer.signal(data);
+		if(peer == null){
+			peer = createPeer();
 		}
+		peer.signal(data);
 	})
 	socket.on('disconnect', ()=>{
 		soc = null;
 	});
 	$("button#init").on('click', ()=>{
-		initiator = true;
-		peer = new SimplePeer({ initiator: true });
-		peer.on('signal', data => {
-			console.log("signal", data);
-			socket.emit('data', data);
-		});
-		peer.on('data', postData);
+		emit("init");
 	});
 	$("button#send").on("click", ()=>{
-		if(typeof peer != 'undefined'){
+		if(peer != null){
 			console.log(`sending data:`, $(this).val());
 			peer.send("test");
 		} else {
 			console.error('peer not ready');
 		}
 	});
+	$("button#close").on('click', destroyRTC);
+	video = document.querySelector('video');
 }
 $(init);
